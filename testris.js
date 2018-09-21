2bb80091b39e0c7d4caf554339e69281f7201665
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-09-21 12:11:54 -0400

    ...

--- testris.js
+++ testris.js
@@ -103,9 +103,9 @@ function update(time = 0){
 document.addEventListener('keydown',event => {
     // console.log(event);
     if(event.keyCode === 37){
-        player.post.x--;
+        player.pos.x--;
     }else if(event.keyCode === 39){
-        player.post.x++;
+        player.pos.x++;
     }else if(event.keyCode === 40){
         playerDrop();
     }
